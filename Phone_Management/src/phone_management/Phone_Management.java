10d54891265c71d0cebdd07f6b52036d9ef4c5cb
ducnam <leducnam209@gmail.com>
2021-11-05 13:17:18 +0700

    sddsds

--- Phone_Management/src/phone_management/Phone_Management.java
+++ Phone_Management/src/phone_management/Phone_Management.java
@@ -16,7 +16,7 @@ public class Phone_Management {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello project 1");
+        System.out.println("vcl");
     }
     
 }
