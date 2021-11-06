47253b73e877499adb3b679a5db4e86210a8af5f
leducnam123 <leducnam209@gmail.com>
2021-11-06 14:22:34 +0700

    change
    Signed-off-by: leducnam123 <leducnam209@gmail.com>

--- Phone_Management/src/phone_management/Phone_Management.java
+++ Phone_Management/src/phone_management/Phone_Management.java
@@ -16,7 +16,7 @@ public class Phone_Management {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("buon");
+        System.out.println("3k");
     }
     
 }
